2723e2361f573968fd74d710445ef351ed89f13e
Jonah Bergevin <jonahbergevin@gmail.com>
2018-10-10 20:50:09 -0600

    I think I pushed the wrong thing up

--- ConcurrencyPlayground.playground/Pages/Hungry Developers.xcplaygroundpage/Contents.swift
+++ ConcurrencyPlayground.playground/Pages/Hungry Developers.xcplaygroundpage/Contents.swift
@@ -3,41 +3,39 @@
 import Foundation
 import Cocoa
 
-// MARK: - Developer Object
+var str = "Hello, playground"
+
 class Developer {
     
-    init(name: String, leftSpoon: Spoon, rightSpoon: Spoon) {
-        self.name = name
-        self.leftSpoon = leftSpoon
-        self.rightSpoon = rightSpoon
-    }
     
-    // MARK: - Properties
-    
-    let name: String
+    let devName: Int
     var leftSpoon: Spoon
     var rightSpoon: Spoon
     
-    
-    // MARK: - Methods
+    init(devName: Int, leftSpoon: Spoon, rightSpoon: Spoon) {
+        self.devName = devName
+        self.leftSpoon = leftSpoon
+        self.rightSpoon = rightSpoon
+    }
     
     func think() {
         DispatchQueue.concurrentPerform(iterations: 2) { int in
             if int == 0 {
                 leftSpoon.pickUp()
-                print("\(name) picked up left spoon")
+                print("\(devName) picked up left spoon")
+                
             } else {
                 rightSpoon.pickUp()
-                print("\(name) picked up right spoon")
+                print("\(devName) picked up right spoon")
             }
         }
     }
     
     func eat() {
-        usleep((arc4random_uniform(8) + 2) * 10000)
+        let _ = usleep((arc4random_uniform(8) + 2) * 10000)
         leftSpoon.putDown()
         rightSpoon.putDown()
-        print("\(name) done eating")
+        print("\(devName) done eating")
     }
     
     func run() {
@@ -46,56 +44,49 @@ class Developer {
             eat()
         }
     }
+    
 }
 
-
-// MARK: - Spoon Object
 class Spoon {
     
-    // MARK: - Properties
+    let spoonIndex: Int
     
-    private var isPickedUp = false
-    private let lock = NSLock()
-    
-    
-    // MARK: - Methods
-    
-    func pickUp() {
-        if isPickedUp {
-            pickUp()
-        } else {
-            lock.lock()
-            print("before changing isPickedUp")
-            isPickedUp = true
-            lock.unlock()
-            return
-        }
+    init(spoonIndex: Int) {
+        self.spoonIndex = spoonIndex
     }
     
-    func putDown() {
-        lock.lock()
-        isPickedUp = false
-        lock.unlock()
+    private let spoonLock = NSLock()
+    
+    func pickUp(){
+        spoonLock.lock()
     }
+    
+    func putDown(){
+        spoonLock.unlock()
+    }
+    
 }
 
+var spoon1 = Spoon(spoonIndex: 1)
+var spoon2 = Spoon(spoonIndex: 2)
+var spoon3 = Spoon(spoonIndex: 3)
+var spoon4 = Spoon(spoonIndex: 4)
+var spoon5 = Spoon(spoonIndex: 5)
 
-// MARK: - Testing
-var spoon1 = Spoon()
-var spoon2 = Spoon()
-var spoon3 = Spoon()
-var spoon4 = Spoon()
-var spoon5 = Spoon()
+var dev1 = Developer(devName: 1, leftSpoon: spoon1, rightSpoon: spoon2)
+var dev2 = Developer(devName: 2, leftSpoon: spoon2, rightSpoon: spoon3)
+var dev3 = Developer(devName: 3, leftSpoon: spoon3, rightSpoon: spoon4)
+var dev4 = Developer(devName: 4, leftSpoon: spoon4, rightSpoon: spoon5)
+var dev5 = Developer(devName: 5, leftSpoon: spoon5, rightSpoon: spoon1)
 
-var developer1 = Developer(name: "Developer1", leftSpoon: spoon1, rightSpoon: spoon2)
-var developer2 = Developer(name: "Developer2", leftSpoon: spoon2, rightSpoon: spoon3)
-var developer3 = Developer(name: "Developer3", leftSpoon: spoon3, rightSpoon: spoon4)
-var developer4 = Developer(name: "Developer4", leftSpoon: spoon4, rightSpoon: spoon5)
-var developer5 = Developer(name: "Developer5", leftSpoon: spoon5, rightSpoon: spoon1)
+let developers = [dev1, dev2, dev3, dev4, dev5]
+var count = 0
 
-let developers = [developer1, developer2, developer3, developer4, developer5]
-
-DispatchQueue.concurrentPerform(iterations: 5) {
-    developers[$0].run()
+DispatchQueue.concurrentPerform(iterations: 5) {_ in
+    for developer in developers {
+        developer.run()
+        count += 1
+        print(count)
+    }
 }
 //: [Next](@next)
